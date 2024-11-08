e12b637ace070fd704aef274048fccdc32fba63a
NK Chauhan <nkchauhan003@gmail.com>
2024-11-08 22:07:24 +0530

    code refactor

--- src/main/java/com/cb/Application.java
+++ src/main/java/com/cb/Application.java
@@ -3,8 +3,10 @@ package com.cb;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 @EnableJpaRepositories("com.cb.repository.postgres")
 public class Application {
 
